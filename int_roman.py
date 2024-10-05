514266a8ed65b7ea81047e8d961fff35ba7b5bad
Tejas-jadhav1 <tejasjadhav1222003@gmail.com>
2024-10-05 22:51:12 +0530

    integer to roman

--- int_roman.py
+++ int_roman.py
@@ -1,12 +1,16 @@
 
-value=[1000,900,500,400,100,90,50,40,10,9,5,4,1]
-num=['M','CM','D','CD','C','XC','L','XL','X','IX','V','IV','I']
+def int_roman(n):
+    value=[1000,900,500,400,100,90,50,40,10,9,5,4,1]
+    num=['M','CM','D','CD','C','XC','L','XL','X','IX','V','IV','I']
+
+    
+    add=""
+    for i in range(len(value)):
+        while n>=value[i]:
+            add=add+num[i]
+            n=n-value[i]
+    
+    print(add)  
 
 n=int(input("Enter the number="))
-add=""
-for i in range(len(value)):
-    while n>=value[i]:
-        add=add+num[i]
-        n=n-value[i]
-    
-print(add)
+int_roman(n)
